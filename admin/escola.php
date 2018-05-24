25a35472936ba00236ff860cc1f79caeb8702684
Aluno <aluno@aluno.com>
2018-05-24 10:26:55 -0400

    Alterando Dados Escola

--- admin/escola.php
+++ admin/escola.php
@@ -1,6 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Alteração Dos Dados</title>
+	<link rel="stylesheet" type="text/css" href="css/escola.css">
+</head>
+<body>
+	<div id="Dados">
+		<label name="NomeEscola"> Nome Da Escola </label>
+		<input type="text" name="NomeEscola">
+		<br>
 
-	 
+		<label name="NumeroTelefone"> N° De Telefone </label>
+		<input type="text" name="NumeroTelefone">
+		<br>
+		<label name="Logo1"> Email </label>
+		<input type="text" name="Logo1">
 
+		<label name="Logo1"> Email </label>
+		<input type="text" name="Logo1">
 
- ?>
+		<a href="#">teste</a>
+	</div>
+</body>
+</html>
