de58682dd6417ff8daba7d8a1da00bd3012a8233
Aluno <aluno@aluno.com>
2018-05-24 12:34:19 -0300

    criação da página de exemplo base para area administrativa

---
+++ admin/includes/head.php
@@ -0,0 +1,18 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>..:: Plataforma Escolar WBS ::..</title>
+    <!-- Bootstrap core CSS-->
+    <link href="components/bootstrap-4.0.1/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom fonts for this template-->
+    <link href="components/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <!-- Custom styles for this template-->
+    <link href="components/sbadmin/css/sb-admin.min.css" rel="stylesheet">
+    
+    <link rel="stylesheet" href="css/adm-estilo.css" type="text/css" />
+    
+    <link rel="shortcut icon" type="image/png" href="imagens/favicon.png"/>
+</head>
---
+++ admin/includes/modal-sair.php
@@ -0,0 +1,18 @@
+<!-- Logout Modal-->
+<div class="modal fade" id="modalSair" tabindex="-1" role="dialog" aria-labelledby="modalSairLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="modalSairLabel">Pronto Para Sair?</h5>
+				<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">×</span>
+				</button>
+			</div>
+			<div class="modal-body">Clique em "Sair" se você realmente deseja sair</div>
+			<div class="modal-footer">
+				<button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
+				<a class="btn btn-primary" href="login.html">Sair</a>
+			</div>
+		</div>
+	</div>
+</div>
